a1ffd6ceabcf7db54da71f48fc602463aa5e9747
KaranamVijayKumar <karanamvijay960@gmail.com>
2019-10-04 23:40:04 -0500

    added FilterActivity files by Vijay Kumar Karanam

--- app/src/main/java/com/example/homefinder/FilterActivity.java
+++ app/src/main/java/com/example/homefinder/FilterActivity.java
@@ -2,7 +2,10 @@ package com.example.homefinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FilterActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class FilterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
+        Button btnFilter = findViewById(R.id.btnFilter);
+        btnFilter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FilterActivity.this,HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
