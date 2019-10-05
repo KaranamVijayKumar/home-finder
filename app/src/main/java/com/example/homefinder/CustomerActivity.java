e43f85382b9a79c2ba8c698d8f34b3fa762b83c2
KaranamVijayKumar <karanamvijay960@gmail.com>
2019-10-05 16:00:55 -0500

    updated links in CustomerActivity.java by Vijay Kumar Karanam

--- app/src/main/java/com/example/homefinder/CustomerActivity.java
+++ app/src/main/java/com/example/homefinder/CustomerActivity.java
@@ -2,7 +2,10 @@ package com.example.homefinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CustomerActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class CustomerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer);
+        Button btnCustomerSignUp = findViewById(R.id.btnCustomerSignUp);
+        btnCustomerSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CustomerActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
