3badaa4c795af408c12f385c6740bc04f1c243c8
KaranamVijayKumar <karanamvijay960@gmail.com>
2019-10-05 15:42:07 -0500

    updated Manifest.xml by Vijay Kumar Karanam

--- app/src/main/java/com/example/homefinder/OwnersActivity.java
+++ app/src/main/java/com/example/homefinder/OwnersActivity.java
@@ -2,7 +2,10 @@ package com.example.homefinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class OwnersActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class OwnersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_owners);
+        Button btnOwner = findViewById(R.id.btnOwner);
+        btnOwner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(OwnersActivity.this,ResultActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
