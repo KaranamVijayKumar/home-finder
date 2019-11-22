c320998b1ed4e884d08132b9270a396c5680b680
KaranamVijayKumar <karanamvijay960@gmail.com>
2019-11-21 20:51:31 -0600

    updated CustomerActivity.java by Vijay Kumar karanam

--- app/src/main/java/com/example/homefinder/CustomerActivity.java
+++ app/src/main/java/com/example/homefinder/CustomerActivity.java
@@ -6,14 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class CustomerActivity extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer);
         Button btnCustomerSignUp = findViewById(R.id.btnCustomerSignUp);
+        final EditText ETcusFN = findViewById(R.id.ETCusFN);
+        final EditText ETcusLN = findViewById(R.id.ETCusLN);
+        final EditText ETcusEmail = findViewById(R.id.ETCusEmail);
+        final EditText ETcusPass = findViewById(R.id.ETCusPass);
+        firebaseAuth = FirebaseAuth.getInstance();
         btnCustomerSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
