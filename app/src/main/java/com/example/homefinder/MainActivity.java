10def9ad98b7892a14cc845c2a140ac76de8d16e
reddylavanya <lavanya27111996@gmail.com>
2019-11-21 19:48:47 -0600

    updated MainActivity.java by Lavanya

--- app/src/main/java/com/example/homefinder/MainActivity.java
+++ app/src/main/java/com/example/homefinder/MainActivity.java
@@ -1,53 +1,58 @@
 package com.example.homefinder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Filter;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText txtEmail, txtPassword;
+    Button btnRegister;
+
+    private FirebaseAuth mAuth;
+    // ...
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btnLogin = findViewById(R.id.btnLogin);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                EditText etUserName = findViewById(R.id.etUserName);
-                EditText etPWD = findViewById(R.id.etPWD);
-                if (etUserName.getText().toString().equals("admin") &&
-                        etPWD.getText().toString().equals("admin")) {
-                    Intent intent = new Intent(MainActivity.this, FilterActivity.class);
-                    startActivity(intent);
-                }else {
-                    Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-        Button btnUserSignUp = findViewById(R.id.btnUserSignUp);
-        btnUserSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, CustomerActivity.class);
-                startActivity(intent);
 
-            }
-        });
-        Button btnSellerSignUp = findViewById(R.id.btnSellerSignUp);
-        btnSellerSignUp.setOnClickListener(new View.OnClickListener() {
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        txtEmail = findViewById(R.id.etEmail);
+        txtPassword = findViewById(R.id.etPWD);
+        btnRegister = findViewById(R.id.btnLogin);
+        // Initialize Firebase Auth
+
+        // initializeUI();
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SellerActivity.class);
-                startActivity(intent);
 
+                // loginUser();
             }
+
         });
 
     }
