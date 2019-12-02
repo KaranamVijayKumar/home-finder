a2a9a9ff75b36072bc6f572d34dda3e8f83c1725
KaranamVijayKumar <karanamvijay960@gmail.com>
2019-12-01 21:07:12 -0600

    updated SellerActivity.java by Vijay kumar Karanam

--- app/src/main/java/com/example/homefinder/SellerActivity.java
+++ app/src/main/java/com/example/homefinder/SellerActivity.java
@@ -1,15 +1,26 @@
 package com.example.homefinder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SellerActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
@@ -29,8 +40,24 @@ public class SellerActivity extends AppCompatActivity {
         btnSellerSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                final String fn,ln,email,pass,role;
+                fn = ETsellFN.getText().toString();
+                ln = ETsellLN.getText().toString();
+                email = ETsellEmail.getText().toString();
+                pass = ETsellPass.getText().toString();
+                role = "SELLER";
 
+                firebaseAuth.createUserWithEmailAndPassword(email, pass)
+                        .addOnCompleteListener(SellerActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                      
+
+                            }
+                        });
+                Intent intent = new Intent(SellerActivity.this,MainActivity.class);
             }
         });
     }
 }
+
