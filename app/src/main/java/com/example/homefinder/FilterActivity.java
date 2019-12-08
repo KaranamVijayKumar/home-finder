524fab132f8db28f38eb2f7a6910ba0d720f1960
Bhuvanesh Nakka <obbybhuvanesh@gmail.com>
2019-12-08 13:01:17 -0600

    updated FilterActivity.java by Bhuvanesh

--- app/src/main/java/com/example/homefinder/FilterActivity.java
+++ app/src/main/java/com/example/homefinder/FilterActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+
 
 public class FilterActivity extends AppCompatActivity {
 
@@ -19,16 +22,36 @@ public class FilterActivity extends AppCompatActivity {
         final EditText maxPrice = findViewById(R.id.etMaxPrice);
         final EditText bedrooms = findViewById(R.id.etBeds);
         final String[] place = {""};
+        final String[][] city = {getResources().getStringArray(R.array.cities)};
         Button btnFilter = findViewById(R.id.btnFilter);
         btnFilter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(FilterActivity.this,HomeActivity.class);
+                intent.putExtra("priceMin",minPrice.getText().toString());
+                intent.putExtra("priceMax",maxPrice.getText().toString());
+                intent.putExtra("city",place[0]);
+                intent.putExtra("bedrooms",bedrooms.getText().toString());
                 startActivity(intent);
             }
         });
+        Spinner cities = findViewById(R.id.spinner);
+        cities.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                place[0] = city[0][position];
+//                Toast.makeText(getApplicationContext(),""+city[position],Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
 }
+
 
 
 
