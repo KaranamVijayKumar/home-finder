826ec233939488ee17cf631ec09203d023f9391c
KaranamVijayKumar <karanamvijay960@gmail.com>
2019-12-08 15:35:37 -0600

    updated homeactivity by Vijay Kumar Karanam

---
+++ app/src/main/java/com/example/homefinder/HomeAdapter.java
@@ -0,0 +1,92 @@
+package com.example.homefinder;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.firestore.DocumentSnapshot;
+
+import java.util.ArrayList;
+
+public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.ViewHolder> {
+    private ArrayList<DocumentSnapshot> values;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView txtHeader;
+        public TextView txtFooter;
+        public TextView txtMiddle;
+
+        public View layout;
+
+        public ViewHolder(View v) {
+            super(v);
+            layout = v;
+            txtHeader =  v.findViewById(R.id.bedroomsValue);
+            txtFooter =  v.findViewById(R.id.priceTV);
+            txtMiddle =  v.findViewById(R.id.ownerTV);
+
+        }
+    }
+
+//    public void add(int position, String item) {
+//        values.add(position, item);
+//        notifyItemInserted(position);
+//    }
+
+//    public void remove(int position) {
+//        values.remove(position);
+//        notifyItemRemoved(position);
+//    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public HomeAdapter(ArrayList<DocumentSnapshot> myDataset) {
+        values = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public HomeAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                     int viewType) {
+        // create a new view
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View v = inflater.inflate(R.layout.home_layout, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        final DocumentSnapshot doc = values.get(position);
+        holder.txtHeader.setText(""+doc.get("bedrooms").toString());
+//        holder.txtHeader.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                remove(position);
+//            }
+//        });
+
+        holder.txtFooter.setText("Price: $"+doc.get("price").toString());
+        holder.txtMiddle.setText("Owner: "+doc.get("owner").toString());
+        Log.d("TAG","Done");
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return values.size();
+    }
+
+}
