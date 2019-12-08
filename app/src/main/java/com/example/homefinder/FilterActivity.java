1b872f563d6612223b4a5fe76b2a6d513d437963
Bhuvanesh Nakka <obbybhuvanesh@gmail.com>
2019-12-08 12:44:40 -0600

    updated FilterActivity.java by Bhuvanesh

--- app/src/main/java/com/example/homefinder/FilterActivity.java
+++ app/src/main/java/com/example/homefinder/FilterActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class FilterActivity extends AppCompatActivity {
 
@@ -13,6 +14,11 @@ public class FilterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
+
+        final EditText minPrice = findViewById(R.id.etMinPrice);
+        final EditText maxPrice = findViewById(R.id.etMaxPrice);
+        final EditText bedrooms = findViewById(R.id.etBeds);
+        final String[] place = {""};
         Button btnFilter = findViewById(R.id.btnFilter);
         btnFilter.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,3 +29,8 @@ public class FilterActivity extends AppCompatActivity {
         });
     }
 }
+
+
+
+
+
