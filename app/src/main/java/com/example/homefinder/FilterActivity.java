68ef260025b887b3372fca2667bd8dba780a0c74
harsha4824 <46695083+harsha4824@users.noreply.github.com>
2019-12-09 01:25:52 -0600

    updated FilterActivity.java by Harsha

--- app/src/main/java/com/example/homefinder/FilterActivity.java
+++ app/src/main/java/com/example/homefinder/FilterActivity.java
@@ -1,5 +1,6 @@
 package com.example.homefinder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,9 +10,22 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 
 public class FilterActivity extends AppCompatActivity {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +37,67 @@ public class FilterActivity extends AppCompatActivity {
         final EditText bedrooms = findViewById(R.id.etBeds);
         final String[] place = {""};
         final String[][] city = {getResources().getStringArray(R.array.cities)};
+//        final ArrayList<DocumentSnapshot> filtered = new ArrayList<>();
+        final ArrayList<Map> docs = new ArrayList<>();
         Button btnFilter = findViewById(R.id.btnFilter);
         btnFilter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(FilterActivity.this,HomeActivity.class);
-                intent.putExtra("priceMin",minPrice.getText().toString());
-                intent.putExtra("priceMax",maxPrice.getText().toString());
-                intent.putExtra("city",place[0]);
-                intent.putExtra("bedrooms",bedrooms.getText().toString());
-                startActivity(intent);
+
+                try {
+                    db.collection("homes").
+                            get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                List<DocumentSnapshot> myListOfDocuments = task.getResult().getDocuments();
+                                for (DocumentSnapshot doc : myListOfDocuments) {
+                                    if (doc.get("city").equals(place[0])) {
+                                        try{
+                                            if (Integer.parseInt(doc.get("bedrooms").toString()) == Integer.parseInt(bedrooms.getText().toString())) {
+                                                if (Integer.parseInt(doc.get("price").toString()) <= Integer.parseInt(maxPrice.getText().toString())
+                                                        && Integer.parseInt(doc.get("price").toString()) >= Integer.parseInt(minPrice.getText().toString())) {
+                                                    if (doc.get("Available").toString().equals("True")) {
+                                                        docs.add(doc.getData());
+
+//                                                        filtered.add(doc);
+                                                    }
+                                        }
+
+//                                    Toast.makeText(getApplicationContext(),doc+"",Toast.LENGTH_LONG).show();
+//                                Toast.makeText(getApplicationContext(),"found",Toast.LENGTH_LONG).show();
+                                            }
+
+                                        }
+                                        catch (Exception e){
+                                            Toast.makeText(getApplicationContext(),"Please choose all filters.",Toast.LENGTH_LONG*4).show();
+
+                                        }
+                                    }
+                                }
+                                if (docs.size() == 0){
+                                    Toast.makeText(getApplicationContext(),"No results found.",Toast.LENGTH_LONG*4).show();
+                                }
+                                else {
+                                    Intent intent = new Intent(FilterActivity.this,HomeActivity.class);
+                                    intent.putExtra("priceMin",minPrice.getText().toString());
+                                    intent.putExtra("priceMax",maxPrice.getText().toString());
+                                    intent.putExtra("city",place[0]);
+                                    intent.putExtra("bedrooms",bedrooms.getText().toString());
+                                    intent.putExtra("docsmap",docs);
+//                                    intent.putExtra("filtered",filtered);
+                                    startActivity(intent);
+                                }
+
+                            }
+                        }
+                    });
+
+
+                }
+                catch (Exception e){
+                    Toast.makeText(getApplicationContext(),"Please choose all filters.",Toast.LENGTH_LONG*4).show();
+                }
             }
         });
         Spinner cities = findViewById(R.id.spinner);
